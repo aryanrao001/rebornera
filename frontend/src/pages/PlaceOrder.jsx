f9375820149c35e62919f525ff619216a77d1d43
aryanrao001 <portfoliorajatyadav@gmail.com>
2024-12-02 16:13:47 +0530

    frontend Complete

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets/frontend_assets/assets'
+import { ShopContext } from '../context/ShopContext'
 
 const PlaceOrder = () => {
 
-  const [method, setMethod] = useState('cod')
+  const [method, setMethod] = useState('cod');
+  const {navigate} = useContext(ShopContext);
+
+
 
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t' >
@@ -58,8 +62,13 @@ const PlaceOrder = () => {
             <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 curson-pointer' >
               <p className={`min-w-3.5 h-3.5 border rounded-full ${ method === 'cod' ? 'bg-green-400' :'' }`} > </p><p className='text-gray-500 text-sm font-medium mx-4' >CASH ON DELIVERY</p>
             </div>
-
           </div>
+
+          <div className='w-full text-end mt-8' >
+            <button onClick={()=>navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm ' >PLACE ORDER</button>
+          </div>
+
+
         </div>
 
       </div>
