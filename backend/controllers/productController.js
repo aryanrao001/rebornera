ac8215d3f6a64a2cb2c9a30fe8470771d4de5119
aryanrao001 <portfoliorajatyadav@gmail.com>
2024-12-10 17:31:37 +0530

    thundercient

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-
+import { v2 as cloudinary } from "cloudinary";
 
 // Function for add product 
 const addProduct = async (req,res) => {
@@ -6,13 +6,27 @@ const addProduct = async (req,res) => {
         
         const {name, description, price, category, subCategory, sizes, bestseller} = req.body;
 
-        const image1 = req.files.image1 && req.files.image1[0]
-        const image2 = req.files.image2 && req.files.image2[0]
-        const image3 = req.files.image3 && req.files.image3[0]
-        const image4 = req.files.image4 && req.files.image4[0]
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1,image2,image3,image4].filter((item)=> item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                return result.secure_url
+            })
+        )
 
         console.log(name, description, price, category, subCategory, sizes, bestseller);
         console.log(image1,image2,image3,image4);
+        console.log(imagesUrl);
+        console.log('req.files:', req.files);
+console.log('req.body:', req.body);
+
+
 
         res.json({}) 
     } catch (error) {
--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,11 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    filename:function(req,res,callback){
+    destination: function (req, file, callback) {
+      callback(null, 'uploads/'); // Ensure this folder exists
+    },
+    filename:function(req , file , callback ){
         callback(null,file.originalname)
     }
 })
 
-const upload = multer ({storage})
+const upload = multer({storage})
 
-export default upload
+export default upload 
