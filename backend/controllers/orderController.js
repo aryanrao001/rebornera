168fdfcd0763543e743be8ea067539bb41fe6e62
aryanrao001 <portfoliorajatyadav@gmail.com>
2024-12-16 19:43:51 +0530

    onlast

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -64,7 +64,14 @@ const userOrders = async(req,res) =>{
 
 // Update order status from Admin Panel
 const updateStatus = async (req,res)=>{
-
+    try {
+        const {orderId , status } = req.body 
+        await orderModel.findByIdAndUpdate(orderId,{ status });
+        res.json({success:true,message:'Status Updated'});
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 export {placeOrder , placeOrderstripe , placeOrderRazorpay ,allOrders, userOrders , updateStatus}
